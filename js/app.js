1d4a5aedc59dbb2e279d5b2142f70d82280ba4c0
Navdeep Walia <lofty.wolf4434@gmail.com>
2020-05-26 11:16:08 +0530

    Adding Master Pages into Website

---
+++ js/app.js
@@ -0,0 +1,4 @@
+fetch('https://api.thecatapi.com/v1/images/search').then(response => response.json().then(data => console.log(data)))
+
+// https://api.thecatapi.com/v1/images/search
+//https://jsonplaceholder.typicode.com/todos/1
